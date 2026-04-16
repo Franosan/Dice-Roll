a3f3d1e885cadfceb881e7fd099a119dccc08e8a
Frano Bakovic <102315962+Franosan@users.noreply.github.com>
2026-04-16 19:01:14 +0200

    Update script.js

--- DiceRoll/script.js
+++ DiceRoll/script.js
@@ -1,41 +1,48 @@
 "use strict";
 
-const btnRoll = document.querySelector(".btn-roll");
 const dice = document.querySelector(".dice");
-const ul = document.querySelector(".ul");
-let diceRoll = 0;
-let rollNumber = 1;
+const roll = document.querySelector(".btn-roll");
+const list = document.querySelector(".ul");
 
-btnRoll.addEventListener("click", function () {
-  rotateDice();
-  const number = Math.trunc(Math.random() * 6) + 1;
-  dice.src = `dice-${number}.png`;
-  addElement(number);
+const object = {
+  diceAnimation: 0,
+  number: 0,
+  rollNumber: 1,
+
+  rollDice() {
+    this.number = Math.trunc(Math.random() * 6) + 1;
+    dice.src = `dice-${this.number}.png`;
+
+    this.diceAnimation += 360;
+    dice.style.rotate = `1 2 1 ${this.diceAnimation}deg`;
+    dice.style.transition = "1s ease-in-out";
+
+    this.addElement();
+  },
+
+  addElement() {
+    const listItem = document.createElement("li");
+    const listContent = document.createTextNode(`Roll ${this.rollNumber++}:`);
+    const listNumber = document.createElement("img");
+
+    list.appendChild(listItem);
+    listItem.append(listContent, listNumber);
+
+    listNumber.src = `dice-${this.number}.png`;
+    listNumber.style.height = "32px";
+
+    listItem.style.fontSize = "20px";
+    listItem.style.backgroundColor = "#47a5c4";
+    listItem.style.color = "#fff";
+    listItem.style.padding = "10px 20px";
+    listItem.style.borderRadius = "10px";
+    listItem.style.marginBottom = "10px";
+    listItem.style.display = "flex";
+    listItem.style.justifyContent = "space-between";
+    listItem.style.alignItems = "center";
+  },
+};
+
+roll.addEventListener("click", function () {
+  object.rollDice();
 });
-
-function rotateDice() {
-  diceRoll += 360;
-  dice.style.rotate = `1 2 1 ${diceRoll}deg`;
-  dice.style.transition = "1s ease-in-out";
-}
-
-function addElement(number) {
-  const listItem = document.createElement("li");
-  const listContent = document.createTextNode(`Roll ${rollNumber++}:`);
-  const listNumber = document.createElement("img");
-  listNumber.src = `dice-${number}.png`;
-  listNumber.style.height = "32px";
-
-  ul.appendChild(listItem);
-  listItem.append(listContent, listNumber);
-
-  listItem.style.fontSize = "20px";
-  listItem.style.backgroundColor = "#47a5c4";
-  listItem.style.color = "#fff";
-  listItem.style.padding = "10px 20px";
-  listItem.style.borderRadius = "10px";
-  listItem.style.marginBottom = "10px";
-  listItem.style.display = "flex";
-  listItem.style.justifyContent = "space-between";
-  listItem.style.alignItems = "center";
-}
